b1cd8aa036f7400b6bcffe399fa482f1f88fc171
tanabee <tanabee1986@gmail.com>
2017-10-06 13:08:18 +0900

    first commit

---
+++ YoutubeClient/UI/VideoList/VideoListVC.swift
@@ -0,0 +1,48 @@
+//
+//  VideoListVC.swift
+//  YoutubeClient
+//
+//  Created by Tanabe Yuki on 2017/10/06.
+//  Copyright © 2017年 Tanabe Yuki. All rights reserved.
+//
+
+import UIKit
+
+class VideoListVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "hello"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.performSegue(withIdentifier: "ShowPlayer", sender: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
